a78b44277aa55c47b8a52330c006f7fd322260c8
ferteles <fernando.teles@tcu.gov.br>
2025-07-22 20:42:26 -0300

    feat: wrap App component with HelmetProvider for improved SEO management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { initBasicSecurity } from "./utils/security";
 import AppRoutes from "./routes";
 import { LanguageProvider } from "./context/LanguageContext";
@@ -9,13 +10,15 @@ function App() {
     initBasicSecurity();
   }, []);
   return (
-    <div className="bg-[#E4D9CD]">
-      <LanguageProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </LanguageProvider>
-    </div>
+    <HelmetProvider>
+      <div className="bg-[#E4D9CD]">
+        <LanguageProvider>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </LanguageProvider>
+      </div>
+    </HelmetProvider>
   );
 }
 
